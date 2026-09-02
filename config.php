0c6747b81aa450fab0cfe08146e3418f9e28d9ba
ramontosea-cell <ramontosea@gmail.com>
2026-09-02 20:48:16 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+$sitename = 'Candid';
+$sitelink = 'Girls';
+
+$target_kota = 'United States';
+
+$Link_instagram = 'https://sailorapart.com/zj5ndeptu?key=b239e5bff36c2a6d4c5cc9c572e6d4c2';
+$Link_twitter = 'https://sailorapart.com/zj5ndeptu?key=b239e5bff36c2a6d4c5cc9c572e6d4c2';
+$Link_tiktok = 'https://sailorapart.com/zj5ndeptu?key=b239e5bff36c2a6d4c5cc9c572e6d4c2';
+$Link_facebook = 'https://sailorapart.com/zj5ndeptu?key=b239e5bff36c2a6d4c5cc9c572e6d4c2';
+
+$Link_OnlyFans = 'https://sailorapart.com/zj5ndeptu?key=b239e5bff36c2a6d4c5cc9c572e6d4c2';
+$Link_Snapchat = 'https://sailorapart.com/zj5ndeptu?key=b239e5bff36c2a6d4c5cc9c572e6d4c2';
+$Link_Telegram = 'https://sailorapart.com/zj5ndeptu?key=b239e5bff36c2a6d4c5cc9c572e6d4c2';
+$Link_Youtube = 'https://sailorapart.com/zj5ndeptu?key=b239e5bff36c2a6d4c5cc9c572e6d4c2';
+$Link_Raffle = 'https://sailorapart.com/zj5ndeptu?key=b239e5bff36c2a6d4c5cc9c572e6d4c2';
+
+?>
